67160a1397701d963a0b0cb4e417bb0dc6db2479
Renren11212 <rentosaiko@gmail.com>
2025-05-01 00:21:21 +0900

    PlayerInputの実装

--- Assets/Tajiri/Scripts/PlayerInputs/PlayerInput.cs
+++ Assets/Tajiri/Scripts/PlayerInputs/PlayerInput.cs
@@ -1,99 +1,35 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-[RequireComponent(typeof(KeyConfigManager))]
 public class PlayerInput : MonoBehaviour
 {
-    // キー設定を管理するマネージャー
-    [SerializeField] private KeyConfigManager _keyConfigManager;
+    private List<IGameAction> _actions = new();
 
-    // 各入力アクションのイベント
-    private Dictionary<InputActionType, System.Action> ActionEvents = new();
-
-    // 現在の入力デバイス（キーボード/ゲームパッド）
-    private IInputDevice _currentInputDevice = new KeyboardInputDevice();
-
-    private void Awake()
+    public void RegisterAction(IGameAction action)
     {
-        _keyConfigManager = GetComponent<KeyConfigManager>();
-        InitializeInputSystem();
+        _actions.Add(action);
     }
 
     private void Update()
     {
-        CheckAllInputs();
-    }
-
-    /// <summary>
-    /// 入力システムの初期化
-    /// </summary>
-    public void InitializeInputSystem()
-    {
-        // 全入力タイプの監視設定
-        foreach (InputActionType actionType in System.Enum.GetValues(typeof(InputActionType)))
+        foreach (var action in _actions)
         {
-            ActionEvents[actionType] = null;
+            CheckAction(action);
         }
     }
 
-    /// <summary>
-    /// 全入力のチェック
-    /// </summary>
-    private void CheckAllInputs()
+    private void CheckAction(IGameAction action)
     {
-        foreach (var actionType in ActionEvents.Keys)
+        bool inputReceived = action.CurrentInputType switch
         {
-            CheckInput(actionType);
+            InputPressType.INSTANT => Input.GetKeyDown(action.CurrentKeyCode),
+            InputPressType.CONTINUOUS => Input.GetKey(action.CurrentKeyCode),
+            _ => false
+        };
+
+        if (inputReceived && action.CanExecute())
+        {
+            action.Execute();
         }
     }
-
-    /// <summary>
-    /// 特定の入力アクションをチェック
-    /// </summary>
-    private void CheckInput(InputActionType actionType)
-    {
-        // キーコンフィグから割り当てキーを取得
-        KeyCode key = _keyConfigManager.GetKeyForAction(actionType);
-
-        // そのキーが押されたとき
-        if (_currentInputDevice.GetKey(key))
-        {
-            ActionEvents[actionType]?.Invoke();
-            //Debug.Log($"Action {actionType} is invoked");
-        }
-    }
-
-    /// <summary>
-    /// 入力アクションを登録（InputActionTypeとメソッドを紐づけるだけ）
-    /// </summary>
-    public void RegisterAction(InputActionType actionType, System.Action callback)
-    {
-        if (ActionEvents.ContainsKey(actionType))
-        {
-            ActionEvents[actionType] += callback;
-        }
-    }
-
-    /// <summary>
-    /// 入力デバイスを切り替え
-    /// </summary>
-    public void SwitchInputDevice(IInputDevice newDevice)
-    {
-        _currentInputDevice = newDevice;
-        Debug.Log($"Input device switched to: {newDevice.GetType().Name}");
-    }
-}
-
-// 入力デバイスインターフェース
-public interface IInputDevice
-{
-    bool GetKeyDown(KeyCode key);
-    bool GetKey(KeyCode key);
-}
-
-// キーボード入力実装(TODO:GetKey想定のアクションかGetKeyDown想定のアクションかどうやって識別しようかな)
-public class KeyboardInputDevice : IInputDevice
-{
-    public bool GetKeyDown(KeyCode key) => Input.GetKeyDown(key);
-    public bool GetKey(KeyCode key) => Input.GetKey(key);
 }
