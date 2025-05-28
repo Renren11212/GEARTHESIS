9183760e9e2f545060fbc96020fd4c3a746f74a1
Negi310 <f.satopec@icloud.com>
2025-05-29 01:31:55 +0900

    修正

--- Assets/Fuji/Scripts/MoveRightAction.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[RequireComponent(typeof(EntityController))]
-public class MoveRightAction : MoveBase
-{
-    public override InputPressType DefaultInputType => InputPressType.CONTINUOUS;
-    public override Vector3 Direction => Vector3.right;
-    public override KeyCode DefaultKeyCode => KeyCode.D;
-}
